86ea3355bcc6532c4c52b1377ce9a375070e522d
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-10-14 18:58:36 +0300

    [Added-RecyclerView-other-LayoutManager]

--- app/src/main/java/com/example/recyclerview_app/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview_app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.recyclerview_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //BURADAKİ ÇALIŞMALAR ÖĞRENME AMAÇLI YAPILMAKTADIR.
 
      var butunManzara = ArrayList<Manzara>()
 
@@ -22,6 +26,31 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = linearLayoutManager
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.anamenu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var id = item.itemId
+
+        when(id) {
+            R.id.linearViewVertical -> {
+                var linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                recyclerView.layoutManager = linearLayoutManager
+            }
+            /*R.id.linearViewHorizontal -> return Diğer Menu Biçicmleri de burada mevcut bulunmaktadır.
+            R.id.straggeredHorizontal -> return
+            R.id.straggeredVertical -> return
+            R.id.GridView -> return
+            */
+        }
+
+        return true
+
+    }
+
 
     fun veriKaynaginiDoldur() : ArrayList<Manzara> { // Bu örnekteki amaç RecyclerView Kullanmak olduğu için buralar statik yaptım
         var tumResimler = arrayOf(R.drawable.manzarabir, R.drawable.manzaraiki)
